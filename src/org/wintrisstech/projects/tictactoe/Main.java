33f2f8da23891d8145eab3092579cc6b5a69174b
Aaron VonderHaar <avh4@Vic-Wintriss-Computer-5.local>
2010-04-25 13:50:29 -0700

    New NetBeans project showing a window.

---
+++ src/org/wintrisstech/projects/tictactoe/Main.java
@@ -0,0 +1,9 @@
+package org.wintrisstech.projects.tictactoe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new TicTacToeGame().getGoing();
+    }
+
+}
---
+++ src/org/wintrisstech/projects/tictactoe/TicTacToeGame.java
@@ -0,0 +1,13 @@
+package org.wintrisstech.projects.tictactoe;
+
+import javax.swing.JFrame;
+
+public class TicTacToeGame {
+
+    void getGoing() {
+        JFrame window = new JFrame("Tic Tac Toe");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
+
+}
